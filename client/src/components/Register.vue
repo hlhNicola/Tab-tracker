41d4c0965323d4bd97bc311f53dc9e557542c5ad
Lihong Han <littlefly14@gmail.com>
2018-08-09 19:54:13 -0500

    second video finished

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,12 +1,21 @@
 <template>
-  <div>
-   <h1>Register</h1>
-   <input type="email" name="email" placeholder="email" v-model="email" />
-   <br>
-   <input type="password" name="password" placeholder="password" v-model="password" />
-   <br>
-   <button @click="register">Register</button>
-  </div>
+  <v-layout> <!-- this part is by Vuetify-->
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+      <div class="pl-4 pr-4 pt-2 pb-2">
+        <input type="email" name="email" placeholder="email" v-model="email" />
+        <br>
+        <input type="password" name="password" placeholder="password" v-model="password" />
+        <br>
+        <div class="error" v-html="error"></div>
+        <v-btn @click="register" class="cyan">Register</v-btn>
+      </div>
+    </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -15,20 +24,27 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      error: null
     }
   },
   methods: {
-    async  register () {
-      const response = await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
-      console.log(response.data)
+    async register () {
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
 </script>
 
 <style scoped>
+.error {
+  color: red
+}
 </style>
--- server/src/app.js
+++ server/src/app.js
@@ -3,16 +3,18 @@ const express = require('express')
 const bodyParser = require('body-parser')// process json data very easily
 const cors = require('cors')
 const morgan = require('morgan')
+const {sequelize} = require('./models')
+const config = require('./config/config')
 
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())// a server can be hosted on a different domain (can be hit from all the world)
 
-app.post('/register', (req, res) => {
-  res.send({
-    message: `Hello ${req.body.email}! You have registered! Have fun!`
-  })
-})
+require('./routes')(app)
 
-app.listen(process.env.PORT || 8081)
+sequelize.sync()
+  .then(() => {
+    app.listen(config.port)
+    console.log(`Server started on port ${config.port}`)
+  })
